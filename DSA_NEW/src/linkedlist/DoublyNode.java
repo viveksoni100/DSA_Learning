6de194b47bd6e801d8a28704f643da5ea0915560
viveksoni100 <viveksoni100@gmail.com>
2022-07-27 16:33:06 +0530

    linked list programs are added

---
+++ DSA_NEW/src/linkedlist/DoublyNode.java
@@ -0,0 +1,7 @@
+package linkedlist;
+
+public class DoublyNode {
+  public int value;
+  public DoublyNode next;
+  public DoublyNode prev;
+}
---
+++ DSA_NEW/src/linkedlist/MainCircularSinglyLinkedList.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class MainCircularSinglyLinkedList {
+  public static void main(String[] args) {
+    CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+    csll.createCSLL(5);
+    csll.insertCSLL(4, 0);
+    csll.insertCSLL(6, 1);
+    csll.insertCSLL(7, 8);
+    csll.traverseCSLL();
+    /*csll.deleteCSLL();
+    csll.traverseCSLL();*/
+
+  }
+}
