7b11d8c8ef59cd5b5a8a3f8bda8e96f4fdc443ca
viveksoni100 <viveksoni100@gmail.com>
2022-07-29 15:41:20 +0530

    stack programs are added

---
+++ DSA_NEW/src/stack/MainStackLinkedList.java
@@ -0,0 +1,14 @@
+package stack;
+
+public class MainStackLinkedList {
+  public static void main(String[] args) {
+    StackArray newStack = new StackArray(5);
+    newStack.push(1);
+    newStack.push(2);
+    newStack.push(3);
+
+    newStack.deleteStack();
+
+
+  }
+}
