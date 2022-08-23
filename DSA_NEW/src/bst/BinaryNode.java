e5f325f7bb886990db2ec285a7fc0540be5f334a
viveksoni100 <viveksoni100@gmail.com>
2022-08-24 01:43:34 +0530

    binary tree programs are done

---
+++ DSA_NEW/src/bst/BinaryNode.java
@@ -0,0 +1,10 @@
+package bst;
+
+public class BinaryNode {
+
+    public int value;
+    public int height;
+    public BinaryNode left;
+    public BinaryNode right;
+
+}
---
+++ DSA_NEW/src/bst/MainBst.java
@@ -0,0 +1,15 @@
+package bst;
+
+public class MainBst {
+
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(120);
+    }
+
+}
