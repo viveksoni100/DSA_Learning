35a82891807607c94c3c6f31f0d074aaeab757e6
viveksoni100 <viveksoni100@gmail.com>
2022-08-30 13:06:46 +0530

    searching algos are added

---
+++ DSA_NEW/src/searching/algorithms/SearchingMain.java
@@ -0,0 +1,9 @@
+package searching.algorithms;
+
+class SearchingMain {
+  public static void main(String[] args) {
+    int[] arr = {8, 9, 12, 15, 17, 19, 20, 21, 28};
+    Searching.binarySearch(arr, 20);
+    
+  }
+}
