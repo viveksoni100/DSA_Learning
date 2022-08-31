9e5dfad5a6fb6d9028b44e84c9e772665b21e7a9
viveksoni100 <viveksoni100@gmail.com>
2022-08-31 19:22:20 +0530

    dijkstra's algorithm is implemented

---
+++ DSA_NEW/src/dijkstras/algo/DijkstrasMain.java
@@ -0,0 +1,30 @@
+package dijkstras.algo;
+
+import java.util.ArrayList;
+
+public class DijkstrasMain {
+
+    public static void main(String[] args) {
+
+        ArrayList<WeightedNode> nodeList = new ArrayList<>();
+        nodeList.add(new WeightedNode("A", 0));
+        nodeList.add(new WeightedNode("B", 1));
+        nodeList.add(new WeightedNode("C", 2));
+        nodeList.add(new WeightedNode("D", 3));
+        nodeList.add(new WeightedNode("E", 4));
+        nodeList.add(new WeightedNode("F", 5));
+        nodeList.add(new WeightedNode("G", 6));
+        WeightedGraph graph = new WeightedGraph(nodeList);
+        graph.addWeightedEdge(0,1,2);
+        graph.addWeightedEdge(0,2,5);
+        graph.addWeightedEdge(1,2,6);
+        graph.addWeightedEdge(1,3,1);
+        graph.addWeightedEdge(1,4,3);
+        graph.addWeightedEdge(2,5,8);
+        graph.addWeightedEdge(2,4,4);
+        graph.addWeightedEdge(4,6,9);
+        graph.addWeightedEdge(5,6,7);
+        System.out.println("printing dijkstra from source: A");
+        graph.dijkstra(nodeList.get(0));
+    }
+}
---
+++ DSA_NEW/src/dijkstras/algo/WeightedGraph.java
@@ -0,0 +1,49 @@
+package dijkstras.algo;
+
+import java.util.*;
+
+public class WeightedGraph {
+    ArrayList<WeightedNode> nodeList = new ArrayList<WeightedNode>();
+
+    public WeightedGraph(ArrayList<WeightedNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    void dijkstra(WeightedNode node) {
+        PriorityQueue<WeightedNode> queue = new PriorityQueue<>();
+        node.distance = 0;
+        queue.addAll(nodeList);
+        while (!queue.isEmpty()) {
+            WeightedNode currentNode = queue.remove();
+            for (WeightedNode neighbor : currentNode.neighbors) {
+                if (queue.contains(neighbor)) {
+                    if (neighbor.distance > currentNode.distance + currentNode.weightMap.get(neighbor)) {
+                        neighbor.distance = (currentNode.distance + currentNode.weightMap.get(neighbor));
+                        neighbor.parent = currentNode;
+                        queue.remove(neighbor); // by doing remove and add, we are refreshing priority queue
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+        for (WeightedNode nodeToCheck : nodeList) {
+            System.out.print("Node " + nodeToCheck + ", distance: " + nodeToCheck.distance + ", Path: ");
+            pathPrint(nodeToCheck);
+            System.out.println();
+        }
+    }
+
+    public static void pathPrint(WeightedNode node) {
+        if (node.parent != null) {
+            pathPrint(node.parent);
+        }
+        System.out.print(node.name + " ");
+    }
+
+    public void addWeightedEdge(int source, int destination, int weight) {
+        WeightedNode first = nodeList.get(source);
+        WeightedNode second = nodeList.get(destination);
+        first.neighbors.add(second);
+        first.weightMap.put(second, weight);
+    }
+}
