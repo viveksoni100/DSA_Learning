5b43b942a1692709087e356551b31e5035490572
viveksoni100 <viveksoni100@gmail.com>
2022-09-12 22:10:18 +0530

    krushkal algorithm is added

---
+++ DSA_NEW/src/krushkal/algo/KruskalMain.java
@@ -0,0 +1,26 @@
+package krushkal.algo;
+
+import java.util.ArrayList;
+
+public class KruskalMain {
+
+    public static void main(String[] args) {
+        ArrayList<WeightedNode> nodeList = new ArrayList<>();
+        nodeList.add(new WeightedNode("A"));
+        nodeList.add(new WeightedNode("B"));
+        nodeList.add(new WeightedNode("C"));
+        nodeList.add(new WeightedNode("D"));
+        nodeList.add(new WeightedNode("E"));
+
+        Kruskal graph = new Kruskal(nodeList);
+        graph.addWeightedUndirectedEdge(0,1,5);
+        graph.addWeightedUndirectedEdge(0,2,13);
+        graph.addWeightedUndirectedEdge(0,4,15);
+        graph.addWeightedUndirectedEdge(1,2,10);
+        graph.addWeightedUndirectedEdge(1,3,8);
+        graph.addWeightedUndirectedEdge(2,3,6);
+        graph.addWeightedUndirectedEdge(2,4,20);
+
+        graph.kruskal();
+    }
+}
---
+++ DSA_NEW/src/krushkal/algo/UndirectedEdge.java
@@ -0,0 +1,24 @@
+package krushkal.algo;
+
+public class UndirectedEdge {
+
+    public WeightedNode first;
+    public WeightedNode second;
+    public int weight;
+
+    public UndirectedEdge(WeightedNode first, WeightedNode second, int weight) {
+        this.first = first;
+        this.second = second;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "UndirectedEdge{" +
+                "first=" + first +
+                ", second=" + second +
+                ", weight=" + weight +
+                '}';
+    }
+
+}
