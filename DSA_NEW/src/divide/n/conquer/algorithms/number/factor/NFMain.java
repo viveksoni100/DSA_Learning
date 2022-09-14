a2fa4a73c66ba6fb4a71869588bba3a1a2972ad1
viveksoni100 <viveksoni100@gmail.com>
2022-09-14 13:00:48 +0530

    number factor programs are added

---
+++ DSA_NEW/src/divide/n/conquer/algorithms/number/factor/NFMain.java
@@ -0,0 +1,8 @@
+package divide.n.conquer.algorithms.number.factor;
+
+public class NFMain {
+    public static void main(String[] args) {
+        NumberFactor numberFactor = new NumberFactor();
+        System.out.println(numberFactor.waysToGetN(4));
+    }
+}
